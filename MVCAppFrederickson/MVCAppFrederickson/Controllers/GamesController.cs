a3d1e14432a56e86761b6646f1f62be200932313
Benjamin Frederickson <benkenfred3@gmail.com>
2019-02-04 21:26:58 -0600

    Completed Assignment. Will check on wednesday if everything is correct.

---
+++ MVCAppFrederickson/MVCAppFrederickson/Controllers/GamesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAppFrederickson.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: BrokenRobot
+        public ActionResult BrokenRobot()
+        {
+            return View("../Games/BrokenRobot/Description");
+        }
+    }
+}
